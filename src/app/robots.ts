21a9924c6ef6edc848f30804a1156df7d6854d3f
ovipep <ovipeps@gmail.com>
2026-08-31 21:00:28 -0400

    Hide site from search engine indexing

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,13 +1,10 @@
 import type { MetadataRoute } from "next";
-import { SITE_URL } from "@/lib/content";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
-      disallow: ["/account/", "/api/", "/checkout/"],
+      disallow: "/",
     },
-    sitemap: `${SITE_URL}/sitemap.xml`,
   };
 }
