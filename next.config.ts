5c5c780e45a0d80f73b414ec1b32ccbdca650658
ovipep <ovipeps@gmail.com>
2026-08-31 21:00:33 -0400

    Hide site from search engine indexing

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     "/api/orders": ["./src/assets/email-attachments/**/*.pdf"],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow, noarchive, nosnippet, noimageindex",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
